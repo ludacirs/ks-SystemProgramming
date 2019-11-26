2368b0d28ae71a46efb25fc0f48404e8ea2053d2
ludacirs <45571631+ludacirs@users.noreply.github.com>
2019-11-26 10:26:54 +0900

    Add 1126

---
+++ 1126/almhandler.c
@@ -0,0 +1,18 @@
+#include<stdio.h>>
+#include<signal.h>
+void alarmHandler();
+
+int main(){
+	signal(SIGALRM,alarmHandler);
+	alarm(5);
+	printf("무한루프\n");
+	while(1){
+		sleep(1);
+		printf("1초경과\n");
+	}
+	printf("실행되지않음\n");
+}
+void alarmHandler(){
+	printf("일어나세요\n");
+	exit(0);
+}
